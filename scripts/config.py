36be89a8705f64f216cbbe8be019de162be64a5b
Mathew Kuthur James <mathewkj2048@gmail.com>
2026-05-04 19:42:13 -0400

    local settings

--- scripts/config.py
+++ scripts/config.py
@@ -31,14 +31,7 @@ def setup(who):
 		print("Nancy's settings")
 		# don't bother with setup each time; just run from sister directory
 		dashplus = "java -ea -jar ../dashplus/app/build/libs/watform-dashplus.jar "
-		sources = [
-			#'../models/case-studies/2019-dash-website/bit-counter/',
-			#'../models/case-studies/2019-dash-website', 
-			#'../models/case-studies/2022-bandali-thesis', 
-			#'../models/case-studies/2022-tamjid-thesis',
-			#'../models/case-studies/2023-bandali-day-paper',
-			'./models/simple-models/',
-			]
+		
 		# verbose = False
 		# stop_on_first_fail = False
 		timeout = 30000 # ms
@@ -46,11 +39,12 @@ def setup(who):
 		num_threads = 1 
 		# a good combination when debugging is verbose=False, stop_on_first_fail=True, num_threads=12
 	elif who == "mkj":
+		print("Mathew's settings")
 		# Mathew can set his own values for the script parameters here
 		dashplus = "java -ea -jar ./dashplus/app/build/libs/watform-dashplus.jar"
-		sources = [
-			'./models/simple-models/'
-			]
+		
+	elif who == "local":
+		dashplus = "java -ea -jar ./dashplus/app/build/libs/watform-dashplus.jar"
 	
 
 
